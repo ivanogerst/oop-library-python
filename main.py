a27c90b51768ba3488511917388357204072cef5
Ivano Gerstman <gerstmanivano@gmail.com>
2025-04-07 03:23:48 +0200

    Testing classes

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-if 2 != 2 or 3:
-    print("a")
+from classes import *
+
+book1 = Book("A Game of Thrones", "George R.R. Martin", 0000, True)
+
+book2 = Book("ASOIAF", "George R.R. Martin", 0000, True)
+
+patron1 = Patron("Steve", "aaaa")
+
+print(patron1.borrowed_books)
+
+patron1.add_books(book1)
+
+patron1.add_books(book2)
+
+patron1.borrowed_books
+
+print(patron1)
