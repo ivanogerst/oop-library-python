1dae05fbed0ae470a2b5fa010719cc01529e96dd
Ivano Gerstman <gerstmanivano@gmail.com>
2025-04-07 02:31:11 +0200

    Added class 'Book'

--- classes.py
+++ classes.py
@@ -19,13 +19,24 @@ class Book():
     #receives two letters, either "T" for True of "F" for False
     def change_status(self, new_status):
         
-        if new_status != "T" or "F":
+        while new_status != "T" or "F":
             
-        
+            new_status = input(f"For book {self.title }, please enter either the letter 'T' if available or 'F' if not available.\n> ")
+            
+        if new_status == "T":
+            
+            new_status = True
+            
+        else:
+            
+            new_status = False
+            
         self.status = new_status
+        
+    
     
     def __str__(self):
         
-        return f"Book title: {self.title}\tAuthor: {self.author}\tStatus: {self.status}"
+        return f"Book title: {self.title}\tAuthor: {self.author}\tStatus: {'Available' if self.status else 'Unavailable'}"
     
     
