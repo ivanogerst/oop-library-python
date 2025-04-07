979d0308939cfadc6710d8ddb78aa1d653ab9f46
Ivano Gerstman <gerstmanivano@gmail.com>
2025-04-07 03:23:10 +0200

    Added __init__() and functions add_books(), remove_books() and __str()__ for class Patron

--- classes.py
+++ classes.py
@@ -1,20 +1,20 @@
 class Book():
     
-    def __init__(self, title, author, unique_number, status):
+    def __init__(self, title, author, identifier, status):
         
         self.title = title
         
         self.author = author
         
-        self.__unique_number = unique_number
+        self.__identifier = identifier
         
         self.status = status
         
         
-    #returns the private varibale unique_number
-    def get_number(self):
+    #returns the private varibale identifier
+    def get_identifier(self):
         
-        return self.__unique_number
+        return self.__identifier
     
     #receives two letters, either "T" for True of "F" for False
     def change_status(self, new_status):
@@ -40,3 +40,43 @@ class Book():
         return f"Book title: {self.title}\tAuthor: {self.author}\tStatus: {'Available' if self.status else 'Unavailable'}"
     
     
+    
+class Patron():
+    
+    def __init__(self, name: str, patron_id: str):
+        
+        self.name = name
+        
+        self.__patron_id = patron_id
+        
+        self.borrowed_books = []
+        
+    def add_books(self, added_book: Book):
+        
+        self.borrowed_books.append(added_book)
+        
+        print(f"Uspješno ste dodali knjigu '{added_book.title}' autora '{added_book.author}'!")
+        
+    def remove_books(self):
+        
+        if len(self.borrowed_books) == 0:
+            
+            print("There are no books that you've borrowed!")
+            
+        else:
+            
+            print("Your books:")
+            
+            for i in range(len(self.borrowed_books)):
+                
+                print(f"\n{i + 1}. {self.borrowed_books[i]}\n")
+            
+            removed_book = int(input("\nPlease enter number of the book that you want to remove: "))
+            
+            print(f"You've succesfully removed '{self.borrowed_books[removed_book - 1].title}' from your cart,")
+            
+            self.borrowed_books.pop(removed_book - 1)
+            
+    def __str__(self):
+        
+        return f"Patron's name: {self.name}\nPatron's borrowed books:\n".join([book.title for book in self.borrowed_books])
